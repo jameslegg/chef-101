b744877cb7ba9fafb34a598053bc904418e4d91c
James Legg <james.legg@conversocial.com>
2015-04-15 16:04:15 +0100

    add apt

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          'all_rights'
 description      'Installs/Configures myfirstcookbook'
 long_description 'Installs/Configures myfirstcookbook'
 version          '0.1.0'
-
+depends          'apt'
